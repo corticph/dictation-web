1191bd92ec81607a60e2176bff00159620fcfb70
Thijs van der Velden <tve@corti.ai>
2025-12-09 11:47:26 +0100

    chore: update recording-button stories

--- stories/recording-button.stories.ts
+++ stories/recording-button.stories.ts
@@ -1,108 +1,86 @@
 import { action } from "@storybook/addon-actions";
-import { html, type TemplateResult } from "lit";
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+import type { RecordingButton } from "../src/components/recording-button.js";
 
 import "../src/components/recording-button.js";
 import "../src/contexts/dictation-context.js";
+import type { DictationContext } from "../src/contexts/dictation-context.js";
+import { disableControls } from "./helpers.js";
 
-export default {
+export type RecordingButtonStory = RecordingButton &
+  Pick<DictationContext, "recordingState">;
+
+const meta = {
   argTypes: {
     preventFocus: {
       control: "boolean",
       description: "Prevent button from taking focus on click",
     },
+    recordingState: {
+      control: "select",
+      description: "The current recording state",
+      options: ["stopped", "initializing", "recording", "stopping"],
+    },
   },
   component: "recording-button",
+  render: ({ preventFocus, recordingState }) => {
+    return html`
+    <dictation-context-provider ?noWrapper=${true} .recordingState=${recordingState}>
+      <recording-button
+        ?preventFocus=${preventFocus}
+        @recording-state-changed=${action("recording-state-changed")}
+        @network-activity=${action("network-activity")}
+        @error=${action("error")}
+      ></recording-button>
+    </dictation-context-provider>
+  `;
+  },
   title: "RecordingButton",
-};
+} satisfies Meta<RecordingButtonStory>;
 
-interface Story<T> {
-  (args: T): TemplateResult;
-  args?: Partial<T>;
-  argTypes?: Record<string, unknown>;
-}
+export default meta;
 
-interface StoryArgs {
-  preventFocus?: boolean;
-}
+export const Default = {
+  args: {
+    preventFocus: true,
+  },
+} as StoryObj<RecordingButtonStory>;
 
-export const Stopped: Story<StoryArgs> = ({ preventFocus }: StoryArgs) => {
-  return html`
-    <dictation-context-provider ?noWrapper=${true}>
-      <recording-button
-        ?preventFocus=${preventFocus}
-        @recording-state-changed=${action("recording-state-changed")}
-        @network-activity=${action("network-activity")}
-        @error=${action("error")}
-      ></recording-button>
-    </dictation-context-provider>
-  `;
-};
-Stopped.args = {
-  preventFocus: true,
-};
+export const Stopped = {
+  args: {
+    preventFocus: true,
+    recordingState: "stopped",
+  },
+  argTypes: disableControls(["recordingState"]),
+} as StoryObj<RecordingButtonStory>;
 
-export const Recording: Story<StoryArgs> = ({ preventFocus }: StoryArgs) => {
-  return html`
-    <dictation-context-provider ?noWrapper=${true} recordingState="recording">
-      <recording-button
-        ?preventFocus=${preventFocus}
-        @recording-state-changed=${action("recording-state-changed")}
-        @network-activity=${action("network-activity")}
-        @error=${action("error")}
-      ></recording-button>
-    </dictation-context-provider>
-  `;
-};
-Recording.args = {
-  preventFocus: true,
-};
+export const Initializing = {
+  args: {
+    preventFocus: true,
+    recordingState: "initializing",
+  },
+  argTypes: disableControls(["recordingState"]),
+} as StoryObj<RecordingButtonStory>;
 
-export const Initializing: Story<StoryArgs> = ({ preventFocus }: StoryArgs) => {
-  return html`
-    <dictation-context-provider ?noWrapper=${true} recordingState="initializing">
-      <recording-button
-        ?preventFocus=${preventFocus}
-        @recording-state-changed=${action("recording-state-changed")}
-        @network-activity=${action("network-activity")}
-        @error=${action("error")}
-      ></recording-button>
-    </dictation-context-provider>
-  `;
-};
-Initializing.args = {
-  preventFocus: true,
-};
+export const Recording = {
+  args: {
+    preventFocus: true,
+    recordingState: "recording",
+  },
+  argTypes: disableControls(["recordingState"]),
+} as StoryObj<RecordingButtonStory>;
 
-export const Stopping: Story<StoryArgs> = ({ preventFocus }: StoryArgs) => {
-  return html`
-    <dictation-context-provider ?noWrapper=${true} recordingState="stopping">
-      <recording-button
-        ?preventFocus=${preventFocus}
-        @recording-state-changed=${action("recording-state-changed")}
-        @network-activity=${action("network-activity")}
-        @error=${action("error")}
-      ></recording-button>
-    </dictation-context-provider>
-  `;
-};
-Stopping.args = {
-  preventFocus: true,
-};
+export const Stopping = {
+  args: {
+    preventFocus: true,
+    recordingState: "stopping",
+  },
+  argTypes: disableControls(["recordingState"]),
+} as StoryObj<RecordingButtonStory>;
 
-export const PreventFocusDisabled: Story<StoryArgs> = ({
-  preventFocus,
-}: StoryArgs) => {
-  return html`
-    <dictation-context-provider ?noWrapper=${true}>
-      <recording-button
-        ?preventFocus=${preventFocus}
-        @recording-state-changed=${action("recording-state-changed")}
-        @network-activity=${action("network-activity")}
-        @error=${action("error")}
-      ></recording-button>
-    </dictation-context-provider>
-  `;
-};
-PreventFocusDisabled.args = {
-  preventFocus: false,
-};
+export const PreventFocusDisabled = {
+  args: {
+    preventFocus: false,
+  },
+} as StoryObj<RecordingButtonStory>;
